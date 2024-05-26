d2cdb1203b0bb22f1ce4401e3c8194a5a7884879
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2024-05-26 16:06:28 +0900

    tRPCを導入

--- mahjong-tools/src/routes/paizu/+page.svelte
+++ mahjong-tools/src/routes/paizu/+page.svelte
@@ -1,23 +1,39 @@
 <script lang="ts">
-    import { PaizuBuilder } from "$lib/image";
-	const hand = [
-		'1m',
-		'1m',
-		'1m',
-		'2m',
-	];
-    let handString = '19m19p19s12345677z';
-    $: paizu = new PaizuBuilder(handString).build();
+	import { PaizuBuilder } from '$lib/image';
+
+	let handString = '19m19p19s12345677z';
+	$: paizu = new PaizuBuilder(handString).build();
+
+	let hands: string[] = [];
+	$: savedPaizus = hands.map((h) => new PaizuBuilder(h).build());
+
+	const handleClick = () => {
+		hands = [...hands, handString];
+	};
 </script>
 
-<div>
-    <div class="flex justify-center items-center gap-0 overflow-x-scroll w-full">
-        {#each paizu as pai}
-            <img src={pai} alt="pai" />
-        {/each}
-    </div>
-    <label class="label">
-        <span>手牌をmpsz表記で入力</span>
-        <input class="input" type="text" placeholder="手牌" bind:value={handString} />
-    </label>
+<div class="flex flex-col items-center gap-2 py-2">
+	<label class="label">
+		<span>手牌をmpsz表記で入力</span>
+		<input class="input" type="text" placeholder="手牌" bind:value={handString} />
+        <div class="flex justify-center items-center gap-0 overflow-x-auto w-full">
+            {#each paizu as pai}
+                <img src={pai} alt="pai" width={24} />
+            {/each}
+        </div>
+	</label>
+
+	<button class="btn variant-ghost-primary" on:click={handleClick}> 保存 </button>
+	<ul class="flex flex-col items-center gap-2">
+		{#each savedPaizus as savedPaizu, index}
+			<li class="flex gap-2">
+                {index}
+				<div class="flex overflow-x-scroll">
+					{#each savedPaizu as pai}
+						<img src={pai} alt="pai" width="24" />
+					{/each}
+                </div>
+			</li>
+		{/each}
+	</ul>
 </div>
--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-alias pnpm='docker compose run -v ${PWD}:${PWD} -w ${PWD} node pnpm'
-alias pd='docker compose run -v ${PWD}:${PWD} -w ${PWD} -p 3000:5173 node pnpm dev --host 0.0.0.0'
+alias pnpm="docker compose run -v ${PWD}:${PWD} -w ${PWD} node pnpm"
+alias pd="docker compose run -v ${PWD}:${PWD} -w ${PWD} -p 3000:5173 node pnpm dev --host 0.0.0.0"
